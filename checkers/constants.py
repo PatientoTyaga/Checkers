8bc824741da69ae1220724f4913ac9df20ac69fb
PatientoTyaga <47284708+PatientoTyaga@users.noreply.github.com>
2023-06-11 05:09:01 -0400

    updating
    adding new board classes and constants

---
+++ checkers/constants.py
@@ -0,0 +1,11 @@
+import pygame
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+#RGB
+RED = (255,0,0) #red color
+WHITE = (255,255,255) #black color
+BLACK = (0,0,0) #black color
+BLUE = (0,0,255) #helps to figure out what box to go to
+
